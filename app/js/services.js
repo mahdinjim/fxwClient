0b8b99d9c45676b4da2410b40ad347ab88491626
mahdinjim <njimmahdi@gmail.com>
2016-05-26 18:59:37 +0300

    updates

--- app/js/services.js
+++ app/js/services.js
@@ -1,6 +1,6 @@
 var services = angular.module('Services',['ngResource','ngCookies']);
 services.factory("Links",[function(){
-	var env="dev";
+	var env="prod";
 	if(env=="dev")
 	{
 		var baseUrl="http://localhost";
